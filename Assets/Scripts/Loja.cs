02bcb94975bc4f487f5c35b75f91aff7780b08eb
Raphael Carrard <70378163+raphaelcarrard@users.noreply.github.com>
2024-03-04 03:07:14 -0300

    Unity 2023

---
+++ Assets/Scripts/Loja.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loja : MonoBehaviour
+{
+
+    public void LojaGo()
+    {
+        NGHelper.instance.unlockMedal(77613);
+        SceneManager.LoadScene(3);
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Delta-Soccer
-A soccer game made with Unity and written in C#,  based on Udemy's 2D Games course
